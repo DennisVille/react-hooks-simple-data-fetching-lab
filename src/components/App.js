956595651b53abbb73d7e7b5e8c05ae28814af6b
dennis <denismukuria@gmail.com>
2025-02-22 02:56:14 +0300

    complete all tasks

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+const App = () => {
+    const [image, setImage] = useState("");
+    const [isReceived, setIsReceived] = useState(false);
+    useEffect(() =>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then ((data) => {
+            setImage(data.message);
+            setIsReceived(true);
+        }) 
+        .catch ((error) => console.error("Error fetching the image:", error));
+    }, []);
+    return (
+        isReceived ? <img src = {image} alt =  "A Random Dog"/> : <p>Loading...</p>
+    );
+};
+export default App;
